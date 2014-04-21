43f3ffb20dbc3e9b2ccb8697d2159de5012fe077
Stephen Chisholm <sbchisholm@gmail.com>
2014-04-21 17:50:29 -0300

    added sub_resource and json

--- src/RestClient.jl
+++ src/RestClient.jl
@@ -25,7 +25,8 @@ module RestClient
   # ---------------------------------------------------------------------------
   # Save an object in a collection either updating the object or creating a new
   # one.
-  function save(api::Api, collection::Collection, data::Dict; id::String="")
+  function save(api::Api, collection::Collection, data::Dict; id::String="",
+                json::Bool=true)
 
       uri = string(api.base_uri, "/", collection.name,
                    id == "" ? "" : string("/", id))
@@ -33,46 +34,45 @@ module RestClient
       if id == ""
           r = post(uri; data = data)
           if r.status == 201
-              return parse(r.data)
+              return json ? parse(r.data) : r.data
           end
       else
           r = put(uri; data = data)
           if r.status == 200
-              return parse(r.data)
+              return json ? parse(r.data) : r.data
           end
       end
 
-      error(string("Unable to save object: ",
-                   parse(r.data)["message"]))
+      error("Save faild: $(r.data)")
   end
 
   # ---------------------------------------------------------------------------
   # Load an object from a collection given the id.
-  function load(api::Api, collection::Collection, id::String)
+  function load(api::Api, collection::Collection, id::String;
+                sub_resource::String="", json::Bool=true)
 
-      uri = string(api.base_uri, "/", collection.name, "/", id)
+      uri = string(api.base_uri, "/", collection.name, "/", id, 
+                   sub_resource != "" ? string("/", sub_resource) : "")
 
       r = get(uri)
       if r.status == 200
-          return parse(r.data)
+          return json ? parse(r.data) : r.data
       end
 
-      error(string("Unable to load object: ",
-                   parse(r.data)["message"]))
+      error("Load failed: $(r.data)")
   end
 
   # ---------------------------------------------------------------------------
   # List objects in a given collection.
-  function list(api::Api, collection::Collection)
+  function list(api::Api, collection::Collection; json::Bool=true)
 
       uri = string(api.base_uri, "/", collection.name)
 
       r = get(uri)
       if r.status == 200
-          return parse(r.data)
+          return json ? parse(r.data) : r.data
       end
 
-      error(string("Unable to list objects: ",
-                   parse(r.data)["message"]))
+      error("List failed: $(r.data)")
   end
 end
